040465cb6bf0aaa35886401e2929c72cfc6e54bf
A. M. Douglas <amd@xmli5.io>
2016-06-07 03:04:19 +0100

    Update toad.js

--- toad.js
+++ toad.js
@@ -1,78 +1,113 @@
 (function(){
-  function toad(config){
-    
-    function addCSSRule(m) {
-      
-      var r=document.querySelectorAll(config.selector),
-          v=r.length,
+
+  var settings;
+
+  window.toad = {
+
+    isInViewport : function(element){
+
+      if( !element || 1 !== element.nodeType ){
+        return false;
+      }
+
+      var r = l.getBoundingClientRect();
+
+      return (
+        !!r
+        && r.bottom > 0
+        && r.right  > 0
+        && r.top    < ( window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight )
+        && r.left   < ( window.innerWidth  || document.documentElement.clientWidth  || document.body.clientWidth  )
+      );
+
+    },
+
+    addCSSRule : function(m){
+
+      var v=document.querySelectorAll(config.selector),
+          r=v.length,
           w=0,
           s=document.styleSheets[0];
-          
-      for(;v>w;w++){
-      	if("insertRule" in s){
-      		s.insertRule(v[w]+"{background-image:url("+m+")}",(s.rules.length));
-      	}
-      	else if("addRule" in s){
-      		s.addRule(v[w],"background-image:url("+m+")",(s.rules.length));
-      	}
-      	else{
-      	  var cssImages=document.createElement("style"),
-          styles=v[w]+"{background-image:url("+m+")}";
-          cssImages.type="text/css";
-          cssImages.setAttribute("scoped",true);
-          cssImages.styleSheet?cssImages.styleSheet.cssText=styles:cssImages.appendChild(document.createTextNode(styles));
-          document.head?document.head.appendChild(cssImages):document.getElementsByTagName("head")[0].appendChild(cssImages);
-      	}
+
+      for (; r > w ; w++)
+      {
+
+        if ( isInViewport( v[w] ) )
+        {
+
+        	if ("insertRule" in s)
+        	{
+        		s.insertRule(v[w]+"{background-image:url("+m+")}",(s.rules.length));
+        	}
+        	else if ("addRule" in s)
+        	{
+        		s.addRule(v[w],"background-image:url("+m+")",(s.rules.length));
+        	}
+        	else
+        	{
+        	  var cssImages=document.createElement("style"),
+            styles=v[w]+"{background-image:url("+m+")}";
+            cssImages.type="text/css";
+            cssImages.styleSheet?cssImages.styleSheet.cssText=styles:cssImages.appendChild(document.createTextNode(styles));
+            document.head?document.head.appendChild(cssImages):document.getElementsByTagName("head")[0].appendChild(cssImages);
+        	}
+
+        }
+
       }
-    }
-    
-    function getImage(m){
-      if(window.fetch){
-        fetch(m)
-        .then(function(response){
-          if (response.ok){
-            addCSSRule(m);
-          }
-          else{
-            console.error("Error: "+response.message);
-          }
-        })
-        .catch(function(error){
-          console.log("Error: "+error.message);
-        });
+
+    },
+
+    applyBgImg : function(){
+
+      var i=settings.images.length,
+          j=0;
+
+      for(;i>j;j++)
+      {
+
+        if(window.addEventListener)
+        {
+            document.addEventListener( "DOMContentLoaded", addCSSRule( i[j], e ), false );
+            window.addEventListener( "load", addCSSRule( i[j], e ), false );
+            window.addEventListener( "scroll", addCSSRule( i[j], e ), false );
+            window.addEventListener( "resize", addCSSRule( i[j], e ), false );
+        }
+        else if(window.attachEvent)
+        {
+            document.attachEvent( "onDOMContentLoaded", addCSSRule( i[j], e ) );
+            window.attachEvent( "onload", addCSSRule( i[j], e ) );
+            window.attachEvent( "onscroll", addCSSRule( i[j], e ) );
+            window.attachEvent( "onresize", addCSSRule( i[j], e ) );
+        }
+        else
+        {
+          window.onload   = addCSSRule( i[j], e);
+          window.onscroll = addCSSRule( i[j], e);
+          window.onresize = addCSSRule( i[j], e);
+        }
+
       }
-      else{
-        var xhr=new XMLHttpRequest();
-        xhr.open("GET",m,true);
-        xhr.onload=function(e){
-          if (xhr.readyState===4){
-            if (xhr.status===(200||304)){
-              addCSSRule(m);
-            }
-            else{
-              console.error(xhr.statusText);
-            }
-          }
-        };
-        xhr.onerror=function(e){
-          console.error(xhr.statusText);
-        };
-        xhr.send(null);
-      }
+
+    },
+
+    init : function(config){
+
+      settings = {
+
+        selector : config && config.selector ? config.selector : ".toad",
+        images   : config && config.images ?
+                   (typeof config.images === "string" ?
+                     config.images.split(",")
+                     : config.images)
+                   : []
+
+      };
+
+      applyBgImg();
+
     }
-    
-    var a=[],
-        b=(typeof config.images === "string" ? config.images.split(",") : config.images),
-        i=a.length,
-        j=0;
-    
-    a.push(b);
-    
-    for(;i>j;j++){
-      getImage(a[j]);
-    }
-  }
-  
-  window.toad=toad;
-  
+
+  };
+
 }());
