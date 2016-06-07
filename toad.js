49666784c96325f0d78d59e00f8cc531bbf4e9ed
A. M. Douglas <amd@xmli5.io>
2016-06-07 22:55:35 +0100

    Update toad.js

--- toad.js
+++ toad.js
@@ -55,7 +55,14 @@
             {
               if ( !!toad.isImg( elements[j] ) )
               {
-                elements[j].src = elements[j].getAttribute( "data-src" );
+                if ( elements[j].src !== elements[j].getAttribute( "data-src" ) )
+                {
+                  elements[j].src = elements[j].getAttribute( "data-src" );
+                }
+                else 
+                {
+                  elements[j].removeAttribute( "data-src" );
+                }
               }
               else
               {
