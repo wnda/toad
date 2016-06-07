32e1770366b579585663e991ccf307e54d68456a
A. M. Douglas <amd@xmli5.io>
2016-06-07 16:48:56 +0100

    Update toad.js

--- toad.js
+++ toad.js
@@ -1,32 +1,30 @@
-;( function (){
-    
-  var settings;
-  
+;( function () {
+
   window.toad = {
-      
+
     isImg : function ( el ) {
-        
+
       if ( "img" === el.tagName ) { return true; }
-      
+
       return false;
-    
+
     },
-          
+
     isInArray : function ( arr, i, item ) {
-        
+
       while ( i-- ) { if ( item === arr[i] ) { return true; } }
-      
+
       return false;
-    
+
     },
-  
+
     isInViewport : function ( element ) {
 
-      if ( !element || 1 !== element.nodeType ) { return false; } 
+      if ( !element || 1 !== element.nodeType ) { return false; }
       else {
-  
+
         var r = element.getBoundingClientRect();
-  
+
         return (
           !!r
           && r.bottom > 0
@@ -37,13 +35,13 @@
       }
 
     },
-    
+
     load : function () {
 
       var elements  = document.querySelectorAll("[data-src]"),
           i         = elements.length,
           j         = 0;
-  
+
       for ( ; i > j; j++)
       {
         if ( !!elements[j].getAttribute( "data-src" ) && !!toad.isInViewport( elements[j] ) )
@@ -54,10 +52,10 @@
             var styles = elements[j].getAttribute("style").split(":"),
                 k      = styles.length;
           }
-              
-          if ( !elements[j].hasAttribute( "style" ) || !toad.isInArray(styles, k, "background" )
+
+          if ( !elements[j].hasAttribute( "style" ) || !toad.isInArray(styles, k, "background" ) )
           {
-            if ( !!toad.isImg( elements[j] ) ) 
+            if ( !!toad.isImg( elements[j] ) )
             {
               elements[j].src = elements[j].getAttribute( "data-src" );
             }
@@ -70,15 +68,15 @@
           {
             elements[j].removeAttribute( "data-src" );
           }
-          
+
         }
-        
+
       }
-      
+
     },
-    
+
     init : function (config) {
-        
+
       if ( window.addEventListener )
       {
           document .addEventListener( "DOMContentLoaded", toad.load, false );
@@ -99,12 +97,11 @@
         window.onscroll = toad.load;
         window.onresize = toad.load;
       }
-      
+
       toad.load();
-      
+
     }
 
   };
-  
-}
-() );
+
+} () );
