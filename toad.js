84232b5c9d0c62d799364b6444be56f7a5385188
A. M. Douglas <amd@xmli5.io>
2016-06-08 01:28:32 +0100

    Update toad.js

--- toad.js
+++ toad.js
@@ -35,7 +35,19 @@
     
     throttleLoad : function () {
       
-      requestAnimationFrame(toad.load);
+      if ( !window.requestAnimationFrame ) {
+      	window.requestAnimationFrame = 
+      	( function () {
+      		return 
+      		   window.webkitRequestAnimationFrame
+      		|| window.mozRequestAnimationFrame
+      		|| window.oRequestAnimationFrame
+      		|| window.msRequestAnimationFrame
+      		|| function( callback, element ) { window.setTimeout( callback, 1000 / 60 ); };
+      	} ) ();
+      }
+      
+      window.requestAnimationFrame(toad.load);
       
     },
 
