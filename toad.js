3e57ac990afecf77b74755ee5dae7ea72bf81f1f
A. M. Douglas <me@amdouglas.com>
2016-08-01 23:50:29 +0100

    Update toad.js

--- toad.js
+++ toad.js
@@ -21,13 +21,15 @@
       if ( !element || 1 !== element.nodeType ) { return false; }
       else 
       {
-        var r = element.getBoundingClientRect();
+        var w = ( window.innerWidth  || document.documentElement.clientWidth  || document.body.clientWidth  ),
+            h = ( window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight ),
+            r = element.getBoundingClientRect();
         return (
           !!r
           && r.bottom > 0
           && r.right  > 0
-          && r.top    < ( window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight )
-          && r.left   < ( window.innerWidth  || document.documentElement.clientWidth  || document.body.clientWidth  )
+          && r.top    < h
+          && r.left   < w
         );
       }
     },
@@ -35,7 +37,9 @@
     debounce : function ( func, wait, scope )
     {
       var timeout;
-      return function () {
+      
+      return function () 
+      {
         var context = scope || this, args = arguments;
         var later = function () {
           timeout = null;
@@ -44,53 +48,55 @@
         clearTimeout( timeout );
         timeout = setTimeout( later, wait );
       };
+      
     },
     
     load : function () 
-    {
+    { // get everything with data-src attribute
       var elements  = document.querySelectorAll( "[data-src]" ),
           i         = elements.length,
           j         = 0;
+      // iterate
       for ( ; i > j; j++)
-      {
+      { // holy shit
         if ( !!elements[j].getAttribute( "data-src" ) && !!toad.isInViewport( elements[j] ) )
-        {
-          if ( elements[j].hasAttribute( "style" ) )
-          {
+        { // has data-src and is in viewport
+          if ( !!elements[j].getAttribute( "style" ) )
+          { // has style attribute set
             var styles = elements[j].getAttribute("style").split(":"),
                 k      = styles.length;
                 
             if ( !toad.isInArray( styles, k, "background-image" ) )
-            {
+            { // has no backgroundImage set
               if ( !!toad.isImg( elements[j] ) )
-              {
+              { // is image
                 elements[j].src = elements[j].getAttribute( "data-src" );
               }
               else
-              {
+              { // is not image and has no background image set
                 elements[j].style.backgroundImage = "url(" + elements[j].getAttribute( "data-src" ) + ")";
               }
             }
             else
-            {
+            { // has background-image already set
               elements[j].removeAttribute( "data-src" );
             }
           }
           else
-          {
+          { // has no style attribute set, but has data-src and is in viewport
             if ( !!toad.isImg( elements[j] ) )
-            {
-              if ( elements[j].getAttribute( "data-src" ) !== elements[j].src  )
-              {
+            { // is image
+              if ( elements[j].getAttribute( "data-src" ) !== elements[j].src )
+              { // src is empty or data-src does not match src attribute
                 elements[j].src = elements[j].getAttribute( "data-src" );  
               }
               else
-              {
+              { // data-src and src are same; remove data-src to prevent rechecking
                 elements[j].removeAttribute( "data-src" );
               }
             }
             else
-            {
+            { // not an image, but is in viewport, has data-src and has no attribute styles
               elements[j].style.backgroundImage = "url(" + elements[j].getAttribute( "data-src" ) + ")";
             }
           }
@@ -101,19 +107,19 @@
     init : function () 
     {
       if ( "addEventListener" in window )
-      {
-          window   .addEventListener( "load",             toad.load, false );
-          window   .addEventListener( "scroll",           toad.debounce( toad.load, 100, this ), false );
-          window   .addEventListener( "resize",           toad.debounce( toad.load, 100, this ), false );
+      { // add events in IE9+
+        window.addEventListener( "load",   toad.load, false );
+        window.addEventListener( "scroll", toad.debounce( toad.load, 100, this ), false );
+        window.addEventListener( "resize", toad.debounce( toad.load, 100, this ), false );
       }
       else if ( "attachEvent" in window )
-      {
-          window   .attachEvent( "onload",             toad.load );
-          window   .attachEvent( "onscroll",           toad.debounce( toad.load, 100, this ) );
-          window   .attachEvent( "onresize",           toad.debounce( toad.load, 100, this ) );
+      { // add events in IE8
+        window.attachEvent( "onload",   toad.load );
+        window.attachEvent( "onscroll", toad.debounce( toad.load, 100, this ) );
+        window.attachEvent( "onresize", toad.debounce( toad.load, 100, this ) );
       }
       else
-      {
+      { // add events in ancient browsers
         window.onload   = toad.load;
         window.onscroll = toad.debounce( toad.load, 100, this );
         window.onresize = toad.debounce( toad.load, 100, this );
