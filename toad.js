801e9cb8a6d26c799c5852de803c71fe463a2269
A. M. Douglas <me@amdouglas.com>
2016-08-02 08:43:54 +0100

    Update toad.js

--- toad.js
+++ toad.js
@@ -1,11 +1,11 @@
-(function (root, factory) {
-    if ( typeof define === 'function' && define.amd ) {
-        define(['buoy'], factory(root));
-    } else if ( typeof exports === 'object' ) {
-        module.exports = factory(require('buoy'));
-    } else {
-        root.myPlugin = factory(root, root.buoy);
-    }
+( function (root, factory) {
+	if ( typeof define === 'function' && define.amd ) {
+		define([], factory(root));
+	} else if ( typeof exports === 'object' ) {
+		module.exports = factory(root);
+	} else {
+		root.toad = factory(root);
+	}
 } )
 ( typeof global !== 'undefined' ? global : this.window || this.global, function (root)
 { // true warriors always use strict
