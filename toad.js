7616737924550f873d6563616f54423c72d56c1d
A. M. Douglas <me@amdouglas.com>
2016-08-23 17:19:30 +0100

    Update toad.js

--- toad.js
+++ toad.js
@@ -1,4 +1,31 @@
+/* https://gist.github.com/paulirish/1579671 */
 ;(function(){
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] 
+                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+ 
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+  
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
+
+/* toad.js */
+(function(){
   
   "use strict";
   
@@ -19,20 +46,6 @@
     }
     return nodeArray;
   };
-  
-  // let's just sneak this in here
-  if( !window.requestAnimationFrame )
-  {
-    window.requestAnimationFrame =
-    (function(){
-      return  window.requestAnimationFrame           ||
-              window.webkitRequestAnimationFrame     ||
-              window.mozRequestAnimationFrame        ||
-              function( callback ){
-                window.setTimeout(callback, 1000 / 60);
-              };
-    })();
-  }
   
   window.toad =
   {
